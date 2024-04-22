4c33fabd9718f6a35506a582eb0e74569bd181d3
salikhdev <salikhdev@gmail.com>
2024-04-22 13:42:10 +0500

    Initial commit

---
+++ app/src/main/java/uz/abdulaziz/smartparking/core/adapter/MainAdapter.kt
@@ -0,0 +1,25 @@
+package uz.abdulaziz.smartparking.core.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import uz.abdulaziz.smartparking.ui.main.location.LocationPage
+import uz.abdulaziz.smartparking.ui.main.profile.ProfilePage
+import uz.abdulaziz.smartparking.ui.main.saved.SavedPage
+
+class MainAdapter(fm: FragmentManager, lc: Lifecycle) : FragmentStateAdapter(fm, lc) {
+
+    override fun getItemCount(): Int {
+        return 2
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> LocationPage()
+            else -> ProfilePage()
+        }
+    }
+
+
+}
